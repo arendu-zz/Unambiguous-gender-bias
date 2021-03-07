c0210a4d8859eef83023f1a6cde8cadb38afb18a
Adi Renduchintala <adirendu@devfair054.maas>
2021-03-07 12:07:16 -0800

    generate script

---
+++ gen.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+__author__ = 'adirendu'
+from nltk.parse.generate import generate
+from nltk import CFG
+for g in ['mofc', 'fomc']:
+    grammar  = CFG.fromstring(open(g + '.grammar', 'r').readlines())
+    w_gen = open(g + '.en', 'w')
+    for sen in generate(grammar):
+        w_gen.write(' '.join(sen) + '\n')
+    w_gen.close()
