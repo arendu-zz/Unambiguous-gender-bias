4cf636bd12ba41f3cd9bba2b0e417ae280c9f0c0
Adi Renduchintala <adirendu@devfair054.maas>
2021-03-25 16:42:53 -0700

    typo

--- eval_scripts/run.sh
+++ eval_scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-tgt=de
+tgt=$1
 python translate.py ../data/mofc.en $tgt > mofc.gen.$tgt
 python tag.py mofc.gen.$tgt $tgt # uses GPU
 python align_and_score.py ../data/mofc.en mofc.gen.$tgt.tok mofc.gen.$tgt.tag ../data/en-$tgt.dict Fem > mofc.gen.$tgt.scores
