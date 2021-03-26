efa2d2ea8a34c368d59903bc4358161dbafb47e9
Adi Renduchintala <adirendu@devfair054.maas>
2021-03-26 16:30:15 -0700

    made small grammars and refac the run script

--- eval_scripts/run.sh
+++ eval_scripts/run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -e
-tgt=$1
-python translate.py ../data/mofc.en $tgt > mofc.gen.$tgt
+if [ $# -ne 3 ]; then
+  echo 1>&2 "Usage: $0 src_file tgt_lang tgt_gen"
+  exit 3
+fi
+echo $1 $2
+src_file=$1
+tgt=$2
+tgt_gen=$3
+python translate.py ${src_file}.en $tgt > ${src_file}.gen.$tgt
 python tag.py mofc.gen.$tgt $tgt # uses GPU
-python align_and_score.py ../data/mofc.en mofc.gen.$tgt.tok mofc.gen.$tgt.tag ../data/occ.en Fem #> mofc.gen.$tgt.scores
+python align_and_score.py ${src_file}.en ${src_file}.gen.$tgt.tok ${src_file}.gen.$tgt.tag ../data/occ.en $tgt_gen > ${src_file}.gen.$tgt.scores
